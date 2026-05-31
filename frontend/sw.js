24555e6b4be94256da0c9745142a779e18f6aaf1
masknetdesign <masknetdesign@gmail.com>
2026-05-30 23:57:20 -0300

    fix: bump service worker cache to v4

--- frontend/sw.js
+++ frontend/sw.js
@@ -1,14 +1,14 @@
 self.addEventListener('install', (e) => {
   self.skipWaiting();
   e.waitUntil(
-    caches.open('keephub-v3').then((cache) => cache.addAll(['/index.html', '/manifest.json']))
+    caches.open('keephub-v4').then((cache) => cache.addAll(['/index.html', '/manifest.json']))
   );
 });
 
 self.addEventListener('activate', (e) => {
   e.waitUntil(
     caches.keys().then((keys) =>
-      Promise.all(keys.filter((k) => k !== 'keephub-v3').map((k) => caches.delete(k)))
+      Promise.all(keys.filter((k) => k !== 'keephub-v4').map((k) => caches.delete(k)))
     )
   );
   return self.clients.claim();
