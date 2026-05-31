da97f4b0aed93fd8bba7dd0f7538cc53e221b83e
masknetdesign <masknetdesign@gmail.com>
2026-05-31 12:58:11 -0300

    feat: complete PWA setup with icons and install button

--- frontend/sw.js
+++ frontend/sw.js
@@ -1,14 +1,16 @@
+const CACHE_NAME = 'keephub-v9';
+
 self.addEventListener('install', (e) => {
   self.skipWaiting();
   e.waitUntil(
-    caches.open('keephub-v8').then((cache) => cache.addAll(['/index.html', '/manifest.json']))
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(['/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png']))
   );
 });
 
 self.addEventListener('activate', (e) => {
   e.waitUntil(
     caches.keys().then((keys) =>
-      Promise.all(keys.filter((k) => k !== 'keephub-v4').map((k) => caches.delete(k)))
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
     )
   );
   return self.clients.claim();
